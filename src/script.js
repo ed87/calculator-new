b5e739af5ac0e1203a04002fc4f68954cac74310
edenkibret2010 <edenkibret2010@gmail.com>
2023-04-23 21:10:05 -0200

    Added some essential parts

--- src/script.js
+++ src/script.js
@@ -18,41 +18,92 @@ document.addEventListener("DOMContentLoaded", function () {
             .replace('log', 'Math.log10')
             .replace('e', 'Math.E')
             .replace('√', 'Math.sqrt')
+            .replace('tan', 'Math.tan')
+            .replace('tan-1', 'Math.atan')
+            .replace('sin-1', 'Math.asin')
+            .replace('cos-1', 'Math.acos')
+            .replace('10^x', '10**')
+            .replace('x^2', '**2')
+            .replace('y√x', '**1/');
+
         console.log('convertedValue: , convertedValue')
         const result = eval(convertedValue);
         currentValue = result.toString();
         display.value = currentValue;
-
     }
-
+    var counter = 0
     for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
         button.addEventListener('click', function () {
             const value = button.innerText;
 
-           try {
-
-         
-            if (value == "AC") {
-                currentValue = "";
-                display.value = currentValue;
-            } else if (value == "=") {
-                evaluateResult();
-            } else {
-                currentValue += value;
-                console.log('currentValue:', currentValue);
-                display.value = currentValue;
-            }
-              
-           }  catch(error){
-                   console.error(error);
-                  currentValue = "ERROR";
-                  display.value =currentValue
+            try {
+                if (value == "AC") {
+                    currentValue = "";
+                    display.value = currentValue;
+                } else if (button.innerText == "=") {
+                    evaluateResult();
                 }
-        
+
+                else if (button.innerText == "Inv") {
+                    const invertibleButtons = document.getElementsByClassName("invertible")
+                    if (counter == 0) {
+
+                        invertibleButtons[0].innerText = "sin-1"
+                        invertibleButtons[1].innerText = "e^x"
+                        invertibleButtons[2].innerText = "cos-1"
+                        invertibleButtons[3].innerText = "10^x"
+                        invertibleButtons[4].innerText = "tan-1"
+                        invertibleButtons[5].innerText = "x^2"
+                        invertibleButtons[6].innerText = "Rnd"
+                        invertibleButtons[7].innerText = "y√x"
+
+                        counter = 1
+                    }
+
+                    else {
+                        invertibleButtons[0].innerText = "sin"
+                        invertibleButtons[1].innerText = "ln"
+                        invertibleButtons[2].innerText = "cos"
+                        invertibleButtons[3].innerText = "log"
+                        invertibleButtons[4].innerText = "tan"
+                        invertibleButtons[5].innerText = "√"
+                        invertibleButtons[6].innerText = "Ans"
+                        invertibleButtons[7].innerText = "x^y"
+
+                        counter = 0
+                    }
+                }
+                else {
+                    currentValue += button.innerText;
+                    display.value = currentValue;
+                }
+
+            } catch (error) {
+                // console.error(error);
+                currentValue = "ERROR";
+                display.value = currentValue
+            }
+
         })
 
 
     }
 
 });
+
+
+var basic = document.getElementById('row1')
+var fx = document.getElementById('row2')
+var num = 0
+const basicOperationBtn = document.getElementById("basic")
+basicOperationBtn.addEventListener('click', function () {
+    basic.classList.remove('d-none')
+    fx.classList.add('d-none')
+})
+const functionalOperationBtn = document.getElementById('fx')
+functionalOperationBtn.addEventListener('click', function () {
+    fx.classList.remove('d-none')
+    basic.classList.add('d-none')
+
+})
