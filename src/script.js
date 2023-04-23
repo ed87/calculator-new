cbf1fa1d1d5aa34a0a2d902db93de915ee293d43
edenkibret2010 <edenkibret2010@gmail.com>
2023-04-23 09:15:08 -0200

    implemented a few scientific function

--- src/script.js
+++ src/script.js
@@ -10,7 +10,14 @@ document.addEventListener("DOMContentLoaded", function () {
         const convertedValue = currentValue
             .replace("×", "*")
             .replace("÷", "/")
-            .replace("%", "*0.01");
+            .replace("%", "*0.01")
+            .replace("sin", "Math.sin")
+            .replace("cos", "Math.cos")
+            .replace('ln', 'Math.log')
+            .replace("π", "Math.PI")
+            .replace('log', 'Math.log10')
+            .replace('e', 'Math.E')
+            .replace('√', 'Math.sqrt')
         console.log('convertedValue: , convertedValue')
         const result = eval(convertedValue);
         currentValue = result.toString();
@@ -23,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener('click', function () {
             const value = button.innerText;
 
+           try {
+
+         
             if (value == "AC") {
                 currentValue = "";
                 display.value = currentValue;
@@ -33,7 +43,15 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.log('currentValue:', currentValue);
                 display.value = currentValue;
             }
+              
+           }  catch(error){
+                   console.error(error);
+                  currentValue = "ERROR";
+                  display.value =currentValue
+                }
+        
         })
+
 
     }
 
